ecd13e8e96bcf9557afb0eb00700a525f029ae59
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2017-12-03 14:11:20 -0500

    Added part of login functionality

--- forms.py
+++ forms.py
@@ -3,20 +3,27 @@ from wtforms import StringField, PasswordField, RadioField, SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class SignupForm(FlaskForm):
-	""" Form for the page where the user signs up """
+	""" Form for the page where the user signs up. """
 	first_name = StringField(label='First Name', id='first_name', validators=[DataRequired('Please enter your first name.')])
 	last_name = StringField(label='Last Name', id='last_name', validators=[DataRequired('Please enter your last name.')])
 	email = StringField(label='Email Address', id='email', validators=[DataRequired('Please enter an email address.'), Email(message='Please enter a valid email address.')])
 	username = StringField(label='Username', id='username', validators=[DataRequired('Please enter a username.')])
 	password = StringField(label='Password', id='password', validators=[DataRequired('Please enter a password.'), Length(min=8, message='Your password must have at least 8 characters.')])
 	confirm_password = PasswordField('Input Password Again')
-	role = RadioField('Role', choices = [('Client'),('Developer')])
+	role = RadioField(label='Role', id='role', validators=[DataRequired('Please choose a role.')], choices = [('client','Client'),('developer','Developer')],)
 	submit = SubmitField('Sign up')
 
-class SigninForm(FlaskForm):
-	""" Form for the page where the user signs in """
+class LoginForm(FlaskForm):
+	""" Form for the page where the user signs in. """
 	username = StringField(label='Username', id='username', validators=[DataRequired('Please enter your username.')])
 	password = StringField(label='Password', id='password', validators=[DataRequired(''), EqualTo('confirm_password', message='The passwords must match.')])
 	submit = SubmitField('Sign in')
+
+class ProtestForm(FlaskForm):
+	""" Form for when user wants to protest a warning. """
+	warning = SelectField(label='Warning to Protest', choices = [('warning1','Warning#1'),('warning2','Warning#2')])
+	reason = TextAreaField(label='Reason for Protest', id='reason', validators=[DataRequired('Please enter a reason for protesting this complaint.')])
+	submit = SubmitField('Submit Protest')
+
 
 
--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from csv import reader
 
 app = Flask(__name__)
 
@@ -20,7 +21,39 @@ def user(name):
 
 @app.route("/apply")
 def apply():
-	return render_template("application.html")
+	return render_template("apply.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	""" 
+	The "/login" route will direct users to the login page if they are not logged in. 
+	If the user is already logged in, they will be redirected to their dashboard.
+	"""
+	# If user is already logged in, redirect them to dashboard
+	if 'username' in session:
+        return redirect(url_for('dashboard'))
+
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+    	username = form.username.data
+        password = form.password.data
+        user_exists = False
+        with open('/database/User.csv', 'r') as f:
+        	csvreader = reader(f, delimiter=',')
+        	for row in csvreader:
+       			if username in row[1]:
+       				user_exists = True
+       	
+    	
+
+	return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """
+    The '/logout' will remove the user from the current session."""
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route("/warning/protest")
 def protestWarning():
