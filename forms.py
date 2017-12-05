b330f55f5f7f3e3c9ed5b5d0b4d9554d12d7af26
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2017-12-04 21:54:55 -0500

    Added static function for checking user id validity

--- forms.py
+++ forms.py
@@ -1,14 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, RadioField, SubmitField, SelectField, TextAreaField, DateField
+from wtforms import StringField, PasswordField, RadioField, SubmitField, SelectField, TextAreaField, DateField, ValidationError
 from wtforms.validators import DataRequired, Email, Length, EqualTo
+from models import Applicant
+
+def validate_user_id(form,field):
+	"""
+	Custom validator for username
+	:returns: True if username is unique and False if username is not unique.
+	"""
+	if not Applicant.is_unique_user_id(field.data):
+		raise ValidationError('User ID is taken. Please enter another User ID.')
 
 class SignupForm(FlaskForm):
-	""" Form for the page where the user signs up. """
+	""" 
+	Form for the page where the user signs up. 
+	"""
 	first_name = StringField(label='First Name', id='first_name', validators=[DataRequired('Please enter your first name.')])
 	last_name = StringField(label='Last Name', id='last_name', validators=[DataRequired('Please enter your last name.')])
 	email = StringField(label='Email Address', id='email', validators=[DataRequired('Please enter an email address.'), Email(message='Please enter a valid email address.')])
 	phone = StringField(label='Phone Number', id='phone', validators=[DataRequired('Please enter a phone number.')])
-	username = StringField(label='Username', id='username', validators=[DataRequired('Please enter a username.')])
+	user_id = StringField(label='User ID', id='user_id', validators=[DataRequired('Please enter a user ID.'), validate_user_id])
 	password = PasswordField(label='Password', id='password', validators=[DataRequired('Please enter a password.'), Length(min=8, message='Your password must have at least 8 characters.')])
 	confirm_password = PasswordField(label='Confirm Password', id ='confirm_password', validators=[DataRequired('Please confirm your password.')])
 	credit_card = StringField(label='Credit Card Number', id='credit_card', validators=[DataRequired('Please enter the credit card number.')])
@@ -20,13 +31,17 @@ class SignupForm(FlaskForm):
 	submit = SubmitField('Sign up')
 
 class LoginForm(FlaskForm):
-	""" Form for the page where the user signs in. """
+	""" 
+	Form for the page where the user signs in. 
+	"""
 	username = StringField(label='Username', id='username', validators=[DataRequired('Please enter your username.')])
 	password = StringField(label='Password', id='password', validators=[DataRequired('Please enter your password.')])
 	submit = SubmitField('Sign in')
 
 class ProtestForm(FlaskForm):
-	""" Form for when user wants to protest a warning. """
+	""" 
+	Form for when user wants to protest a warning. 
+	"""
 	warning = SelectField(label='Warning to Protest', choices = [('warning1','Warning#1'),('warning2','Warning#2')])
 	reason = TextAreaField(label='Reason for Protest', id='reason', validators=[DataRequired('Please enter a reason for protesting this complaint.')])
 	submit = SubmitField('Submit Protest')
